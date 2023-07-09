691831678b035182dc172e2be863990d23e6bd5f
hpearson98 <harry.m.pearson@gmail.com>
2023-07-09 15:06:40 +0000

    Added site pagination and the ability to view post details

--- blog/admin.py
+++ blog/admin.py
@@ -18,6 +18,10 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
 
 
 # Register your models here.
